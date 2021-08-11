2b5a544de36222d6cc17d94e80c64eeca0ccf6d1
antoniosumak <sumakantonio@gmail.com>
2021-08-11 22:40:26 +0200

    done application

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :style="{ background: buttonColor }" class="btn " @click="onClick()">
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    buttonText: String,
+    buttonColor: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit("toggle-add-task");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
